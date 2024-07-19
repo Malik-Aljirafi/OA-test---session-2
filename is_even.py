2f747052a4320853aade6fa9682f3eabdb80f141
Malik Aljirafi <malikaljirafi@MacBook-Pro.local>
2024-07-18 23:43:45 -0400

    added new is_odd function

--- is_even.py
+++ is_even.py
@@ -5,3 +5,6 @@ Function that returns True if the number is even, False otherwise.
 def is_even(number: int) -> bool:
     return number % 2 == 0
 
+
+def is_odd(number: int) -> bool:
+    return number % 2 != 0
