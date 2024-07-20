83df89efd9968a66ad9f97df28bedb1b905a63ee
Malik Aljirafi <malikaljirafi@MacBook-Pro.local>
2024-07-19 21:57:00 -0400

    fix

--- is_even.py
+++ is_even.py
@@ -6,5 +6,8 @@ def is_even(number: int) -> bool:
     return number % 2 == 0
 
 
+'''
+Function that returns True if the number is odd, False otherwise.
+'''
 def is_odd(number: int) -> bool:
     return number % 2 != 0
